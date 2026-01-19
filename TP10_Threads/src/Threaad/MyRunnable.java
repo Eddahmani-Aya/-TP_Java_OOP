0374a12a0c08e06a56cf643a9f4b4e6b028dc42a
Eddahmani Aya <edaya816@gmail.com>
2026-01-19 21:57:42 +0100

    💻 TP_Java_OOP

---
+++ TP10_Threads/src/Threaad/MyRunnable.java
@@ -0,0 +1,23 @@
+package Threaad;
+
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		
+		for (int i = 1; i <=5; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.out.println("Threads was Interrupted ");
+			}
+			if (i==5) {
+				System.out.println("Time's up");
+				System.exit(0);
+				
+			}
+		}
+		
+	}
+
+}
